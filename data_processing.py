2af84d871efdeb4709de26333beae31f6db192c8
nghiab1302214 <nghiab1302214@gmail.com>
2025-12-14 21:37:02 +0700

    update data processing

--- data_processing.py
+++ data_processing.py
@@ -1,2 +1,6 @@
 
 print("adding all the data processing code related fraud detection")
+print("data cleaning functions")
+print("feature engineering functions")
+print("data normalization functions")
+print("data splitting functions")
